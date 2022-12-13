b710d027a3763919ca6523a2d5ece4d8211dfa5f
Ayush0Chaudhary <ayush0000ayush@gmail.com>
2022-12-14 03:52:07 +0530

    chor: Make some changes in models.

--- rasputin/models.py
+++ rasputin/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Topic(models.Model):
@@ -8,20 +9,13 @@ class Topic(models.Model):
         return self.name
 
 class Room(models.Model):
-    host = models.ForeignKey("Users", on_delete=models.SET_NULL, null=True)
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey("Topic", on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
-    # participants = models.ManyToManyField("Users")
+    # participants = models.ManyToManyField("User")
     created = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.name
-
-class Users(models.Model):
-    name = models.CharField(max_length=50)
-    created = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name    
