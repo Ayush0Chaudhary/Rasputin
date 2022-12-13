e68e7c0f41defa49cc0e1cc5daf3804041fbb4ee
Ayush0Chaudhary <ayush0000ayush@gmail.com>
2022-12-14 03:28:38 +0530

    feat: Add models for users, topic, room.

--- rasputin/models.py
+++ rasputin/models.py
@@ -1,11 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return self.name
+
 class Room(models.Model):
-    # host = models.
-    topic = models.TextField(null=True, blank=True)
+    host = models.ForeignKey("Users", on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey("Topic", on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
-    # participants
+    # participants = models.ManyToManyField("Users")
     created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+class Users(models.Model):
+    name = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name    
--- rasputin/views.py
+++ rasputin/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Room
 
 def home(request):
     return render(request, template_name='home.html')
+
 def room(request, pk):
-    return render(request=request, template_name='room.html', context={'num': pk})
+    room = Room.objects.get(id= pk)
+    return render(request=request, template_name='room.html', context={"room":room})
